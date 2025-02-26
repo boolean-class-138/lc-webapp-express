f078e0ec3bcc9953d22940c6a0c95bc1d2f9b541
samuelemadrigali <samuele.madrigali@gmail.com>
2025-02-26 13:01:02 +0100

    feat: add images url

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,14 +3,14 @@ const connection = require("../data/db");
 
 // Index
 const index = (req, res) => {
-  const sql = "SELECT * FROM books";
+  // const sql = "SELECT * FROM books";
 
   // BONUS: query con media voto per ogni libro
-  // const sql = `
-  // SELECT books.*, ROUND(AVG(reviews.vote)) as avg_vote
-  // FROM books
-  // LEFT JOIN reviews ON books.id = reviews.book_id
-  // GROUP BY books.id`;
+  const sql = `
+  SELECT books.*, ROUND(AVG(reviews.vote)) as avg_vote
+  FROM books
+  LEFT JOIN reviews ON books.id = reviews.book_id
+  GROUP BY books.id`;
 
   // lancio la query
   connection.execute(sql, (err, results) => {
@@ -21,7 +21,12 @@ const index = (req, res) => {
       });
     }
 
-    res.json(results);
+    const books = results.map((book) => {
+      book.image = `${process.env.BE_URL}/books/${book.image}`;
+      return book;
+    });
+
+    res.json(books);
   });
 };
 
@@ -53,6 +58,9 @@ const show = (req, res) => {
         message: "Book not found",
       });
     }
+
+    // Modifico la proprietà image aggiungendo l'url completa
+    book.image = `${process.env.BE_URL}/books/${book.image}`;
 
     // query per recuperare le recensioni di quel libro
     const reviewsSql = `
